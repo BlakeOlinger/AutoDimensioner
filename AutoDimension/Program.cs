d2a124bbed94aa11844c2175883c86eb43565101
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-10 14:22:16 -0400

    Add project files.

---
+++ AutoDimension/Program.cs
@@ -0,0 +1,24 @@
+﻿using SldWorks;
+using SwConst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDimension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swInstance = new SldWorks.SldWorks();
+
+            var model = (ModelDoc2)swInstance.ActiveDoc;
+            var drawing = (DrawingDoc)model;
+
+            drawing.InsertModelAnnotations3((int)swImportModelItemsSource_e.swImportModelItemsFromEntireModel, 163840, true, true, true, false);
+
+        }
+    }
+}

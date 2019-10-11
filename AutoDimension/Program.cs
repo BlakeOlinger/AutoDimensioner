aa937f9bd9279b803c704f57a1e625817edf8c7a
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-11 07:39:21 -0400

    -- automated unsuppress after drawing dimensions

--- AutoDimension/Program.cs
+++ AutoDimension/Program.cs
@@ -1,11 +1,6 @@
 ﻿using SldWorks;
 using SwConst;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AutoDimension
 {
@@ -16,7 +11,8 @@ namespace AutoDimension
             var swInstance = new SldWorks.SldWorks();
             
             // open blob.L2_cover.SLDASM
-            var model = (ModelDoc2)swInstance.OpenDoc("C:\\Users\\bolinger\\Documents\\SolidWorks Projects\\Prefab Blob - Cover Blob\\blob - L2\\blob.L2_cover.SLDASM", (int)swDocumentTypes_e.swDocASSEMBLY);
+            var coverAssemblyPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\blob - L2\blob.L2_cover.SLDASM";
+            var model = (ModelDoc2)swInstance.OpenDoc(coverAssemblyPath, (int)swDocumentTypes_e.swDocASSEMBLY);
 
             // wait for document to open
             Thread.Sleep(4_000);
@@ -34,10 +30,10 @@ namespace AutoDimension
 
             // wait for write
             Thread.Sleep(1_000);
-
+            
             // rebuild to reflect cover config write
             model.ForceRebuild3(false);
-
+            
             // wait for a few seconds
             Thread.Sleep(2_000);
 
@@ -51,15 +47,38 @@ namespace AutoDimension
             // draw dimensions
             model = (ModelDoc2)swInstance.ActiveDoc;
             var drawing = (DrawingDoc)model;
-
             drawing.InsertModelAnnotations3((int)swImportModelItemsSource_e.swImportModelItemsFromEntireModel, 163840, true, true, true, false);
-            // TODO - finish the unsuppres the cover assembly and save step
 
-            // TODO - make this program open blob.L2_cover.SLDASM -> write '0' to the "Is Dimensioned" field
-            // of blob.L2_cover.txt -> build the cover assembly to suppress the items -> open the drawing document in SW
-            // -> call this program's drawing.InsertModelAnnotations3() -> switch active docs to the assembly ->
-            // write '1' to the "Is Dimensioned" field of blob.L2_cover.txt -> save and close that document ->
-            // continue with the autoDrawing tasks from other programs
+            // wait a few seconds
+            Thread.Sleep(2_000);
+
+            // switch to assembly document
+            var frame = (Frame)swInstance.Frame();
+            var windows = (object[])frame.ModelWindows;
+            var assemblyWindow = (ModelWindow)windows[1];
+            frame.ShowModelWindow(assemblyWindow);
+            
+            // write unsuppress to assembly config
+            coverConfigLines = System.IO.File.ReadAllLines(coverConfigPath);
+            isDimensionedLine = coverConfigLines[30];
+            replacedLine = isDimensionedLine.Replace("0", "1");
+            coverConfigLines[30] = replacedLine;
+            System.IO.File.WriteAllLines(coverConfigPath, coverConfigLines);
+
+            // wait a few seconds
+            Thread.Sleep(2_000);
+
+            // rebuild to unsuppress assembly - necessary to unsuppress open drawing
+            model = (ModelDoc2)swInstance.ActiveDoc;
+            model.ForceRebuild3(false);
+            Thread.Sleep(2_000);
+            model.ForceRebuild3(false);
+
+            // wait a few seconds
+            Thread.Sleep(2_000);
+
+            // close assembly file
+            swInstance.CloseDoc(coverAssemblyPath);
         }
     }
 }
